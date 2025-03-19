b809e29252c3d7adf409d651fe73f2e411dd8447
Mahalakshmi60-creator <mkrjptpt@gmail.com>
2025-03-19 11:48:59 +0530

    Add files via upload

---
+++ Experiment 6/Abstract class/TestGameCharacter.java
@@ -0,0 +1,27 @@
+
+abstract class GameCharacter {
+    abstract void attack();
+}
+
+
+class Warrior extends GameCharacter {
+    void attack() {
+        System.out.println("Warrior attacks with a sword!");
+    }
+}
+
+class Mage extends GameCharacter {
+    void attack() {
+        System.out.println("Mage attacks with a fireball!");
+    }
+}
+
+public class TestGameCharacter {
+    public static void main(String[] args) {
+        GameCharacter warrior = new Warrior();
+        GameCharacter mage = new Mage();
+        
+        warrior.attack();
+        mage.attack();
+    }
+}
