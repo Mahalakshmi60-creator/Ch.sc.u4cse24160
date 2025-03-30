f84f47accc651cee088a9dc62111a17c689f3f58
Mahalakshmi60-creator <mkrjptpt@gmail.com>
2025-03-30 12:54:51 +0530

    Add files via upload

---
+++ Experiment 9/Built-in package/Random.java
@@ -0,0 +1,14 @@
+import java.util.Random; // Importing built-in package
+
+public class Random {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        System.out.println("Random Number: " + rand.nextInt(100));
+    }
+}
+
+
+
+
+
+
---
+++ Experiment 9/User defined package/example 2/Calculator.java
@@ -0,0 +1,11 @@
+package mathoperations; // Same package
+
+public class AdvancedCalculator {
+    public double power(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
+}
+
+
+
+
